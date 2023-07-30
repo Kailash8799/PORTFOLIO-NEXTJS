419986bbe5aa10ba9394ae0a27d8d0d4a30ae8c2
kailash8799 <kailashrajput8799@gmail.com>
2023-07-30 21:40:54 +0530

    new portfolio

---
+++ src/components/BottomItem.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import React, { useState } from "react";
+
+const BottomItem = ({ img, size, tooltipname }) => {
+  const [tooltip, settooltip] = useState(false);
+  return (
+    <div>
+      <div className="relative">
+        {tooltip && (
+          <div className="absolute py-2 text-gray-300 rounded-lg w-28 -left-8 -top-14 bg-gray-950/30">
+            {tooltipname}
+          </div>
+        )}
+        <Image
+          src={img}
+          className="transition-all cursor-pointer hover:scale-125"
+          alt="Compass Image"
+          onMouseEnter={() => {
+            settooltip(true);
+          }}
+          onMouseOut={() => {
+            settooltip(false);
+          }}
+          width={size}
+          height={size}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default BottomItem;
---
+++ src/components/TopNavbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TopNavbar = () => {
+  return (
+    <div className='sticky top-0 h-10 bg-slate-700/20'>TopNavbar</div>
+  )
+}
+
+export default TopNavbar
